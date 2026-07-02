6a65c0e19d8f1fcd444a3c11290fe52750c8f317
anshul rastogi <anshulrastogi@Anshuls-MacBook-Pro.local>
2026-07-02 23:57:09 +0530

    Module 3: Stored Procedures

---
+++ Week 1/StoredProcedures/Scenario2.sql
@@ -0,0 +1,15 @@
+ALTER TABLE Employees ADD Department VARCHAR2(50);
+
+CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus (
+    p_department IN VARCHAR2,
+    p_bonus_percentage IN NUMBER
+) IS
+BEGIN
+
+    UPDATE Employees
+    SET Salary = Salary + (Salary * (p_bonus_percentage / 100))
+    WHERE Department = p_department;
+    
+    COMMIT;
+END UpdateEmployeeBonus;
+/
---
+++ Week 1/StoredProcedures/Scenario3.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE PROCEDURE TransferFunds (
+    p_from_account IN NUMBER,
+    p_to_account IN NUMBER,
+    p_amount IN NUMBER
+) IS
+    v_current_balance NUMBER;
+BEGIN
+
+    SELECT Balance INTO v_current_balance
+    FROM Accounts
+    WHERE AccountID = p_from_account;
+
+    IF v_current_balance >= p_amount THEN
+        UPDATE Accounts
+        SET Balance = Balance - p_amount
+        WHERE AccountID = p_from_account;
+
+        UPDATE Accounts
+        SET Balance = Balance + p_amount
+        WHERE AccountID = p_to_account;
+
+        COMMIT;
+        DBMS_OUTPUT.PUT_LINE('Transfer successful.');
+    ELSE
+
+        DBMS_OUTPUT.PUT_LINE('Error: Insufficient funds for transfer.');
+    END IF;
+
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Error: One or both account IDs do not exist.');
+END TransferFunds;
+/
