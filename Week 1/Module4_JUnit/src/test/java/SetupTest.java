65228a925acc0fdd9f8865fef9542d9a82776d9e
anshul rastogi <anshulrastogi@Anshuls-MacBook-Pro.local>
2026-07-03 03:39:01 +0530

    Module 4: Successfully ran JUnit setup test

---
+++ Week 1/Module4_JUnit/src/test/java/SetupTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class SetupTest {
+
+    @Test
+    public void testJUnitSetup() {
+        System.out.println("JUnit is successfully set up and running!");
+        assertTrue("This should always be true", true);
+    }
+}
